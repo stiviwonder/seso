99533d38d49a96bc37720048a7c9d81719261b9b
stiviwonder <sevicar3@gmail.com>
2020-10-30 11:20:06 +0100

    added clean and run functions

--- lmake.lua
+++ lmake.lua
@@ -9,3 +9,16 @@ function build()
     lmake_set_linker_out("seso")
     lmake_link("bin/main.c.o bin/func.c.o bin/clock.c.o bin/timer.c.o bin/p_gen.c.o bin/scheduler.c.o")
 end
+
+function clean()
+    lmake_exec("rm -rf bin/main.c.o")
+    lmake_exec("rm -rf bin/clock.c.o")
+    lmake_exec("rm -rf bin/timer.c.o")
+    lmake_exec("rm -rf bin/p_gen.c.o")
+    lmake_exec("rm -rf bin/scheduler.c.o")
+    lmake_exec("rm -rf bin/func.c.o")
+end
+
+function run()
+    lmake_exec("seso")
+end
