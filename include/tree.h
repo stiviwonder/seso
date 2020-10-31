33fe5092fad8ec088d1b5b9c49e5557b4459c3f8
stiviwonder <sevicar3@gmail.com>
2020-10-31 13:50:21 +0100

    added tree headers

---
+++ include/tree.h
@@ -0,0 +1,5 @@
+node_t* new_node(int vruntime, struct process p, node_t* dad);
+node_t* find_minimum(node_t* root);
+node_t* insert(node_t* root, int x, struct process p, node_t* dad);
+node_t* delete(node_t* root, int x);
+void print_tree(node_t* root);
