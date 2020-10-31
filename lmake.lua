79d55a31a435cbb7e260da32e5220e4416e811b2
stiviwonder <sevicar3@gmail.com>
2020-10-31 13:49:36 +0100

    binary and compiler

--- lmake.lua
+++ lmake.lua
@@ -3,11 +3,11 @@ function build()
     lmake_set_compiler("/bin/gcc")
     lmake_set_compiler_flags("-pthread -Wall -Iinclude")
     lmake_set_compiler_out("bin/%.o")
-    lmake_compile("src/main.c src/func.c src/clock.c src/timer.c src/p_gen.c src/scheduler.c")
+    lmake_compile("src/main.c src/func.c src/clock.c src/timer.c src/p_gen.c src/scheduler.c src/tree.c")
     lmake_set_linker("/bin/gcc")
     lmake_set_linker_flags("-pthread")
     lmake_set_linker_out("seso")
-    lmake_link("bin/main.c.o bin/func.c.o bin/clock.c.o bin/timer.c.o bin/p_gen.c.o bin/scheduler.c.o")
+    lmake_link("bin/main.c.o bin/func.c.o bin/clock.c.o bin/timer.c.o bin/p_gen.c.o bin/scheduler.c.o bin/tree.c.o")
 end
 
 function clean()
@@ -17,6 +17,7 @@ function clean()
     lmake_exec("rm -rf bin/p_gen.c.o")
     lmake_exec("rm -rf bin/scheduler.c.o")
     lmake_exec("rm -rf bin/func.c.o")
+    lmake_exec("rm -rf bin/tree.c.o")
 end
 
 function run()
