0cce3e96a79362b9a5f71230a81907d4972b3831
stiviwonder <sevicar3@gmail.com>
2020-11-13 18:55:08 +0100

    changed script

--- lmake.lua
+++ lmake.lua
@@ -5,16 +5,16 @@ link_files = lmake_find("bin/*.o");
 function build()
     lmake_set_compiler("/bin/gcc")
     lmake_set_compiler_flags("-pthread -Wall -Iinclude")
-    lmake_set_compiler_out("bin/%.o")
+    lmake_set_compiler_out("build/%.o")
     lmake_compile("src/main.c src/func.c src/clock.c src/timer.c src/p_gen.c src/scheduler.c src/tree.c src/queue.c")
     lmake_set_linker("/bin/gcc")
     lmake_set_linker_flags("-pthread")
     lmake_set_linker_out("seso")
-    lmake_link("bin/main.c.o bin/func.c.o bin/clock.c.o bin/timer.c.o bin/p_gen.c.o bin/scheduler.c.o bin/tree.c.o bin/queue.c.o")
+    lmake_link("build/main.c.o build/func.c.o build/clock.c.o build/timer.c.o build/p_gen.c.o build/scheduler.c.o build/tree.c.o build/queue.c.o")
 end
 
 function clean()
-    lmake_exec("rm -rf bin/main.c.o bin/func.c.o bin/clock.c.o bin/timer.c.o bin/p_gen.c.o bin/scheduler.c.o bin/tree.c.o bin/queue.c.o")
+    lmake_exec("rm -rf build/main.c.o build/func.c.o build/clock.c.o build/timer.c.o build/p_gen.c.o build/scheduler.c.o build/tree.c.o build/queue.c.o")
 end
 
 function run()
