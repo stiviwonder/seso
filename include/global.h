c41cd5e909f3b76dbbda874d5ebde6bdcf85b2e9
stiviwonder <sevicar3@gmail.com>
2020-11-17 16:19:43 +0100

    new definitions

--- include/global.h
+++ include/global.h
@@ -9,13 +9,13 @@ typedef struct process{
     long pid;
     int vruntime;
     int time;
-    int exec_time;
 }process_t;
 
 struct core{
     int id;
     process_t execution;
     int executing;
+    int exec_time;
 };
 
 typedef struct cpu{
@@ -23,13 +23,35 @@ typedef struct cpu{
     int core_kop;
 }cpu_t;
 
+typedef struct mm{
+    int pgb;	// Orri taularen helbide fisikoa
+    int code;	// Kodearen helbide birtuala
+    int data;	// Datuen helbide birtuala
+}mm_t;
+
 
 typedef struct node{
     struct process process;
+    mm_t mem_m;
     struct node* right;
     struct node* left;
     
 }node_t;
+
+typedef struct mmu{
+}mmu_t;
+
+typedef struct tlb{
+}tlb_t;
+
+typedef struct ptbr{
+}ptbr_t;
+
+typedef struct ir{
+}ir_t;
+
+typedef struct pc{
+}pc_t;
 
 /*====== GLOBAL VARIABLES ======*/
 
