02653ac0b014b4071bb8a2ff6d0efb802919dddd
stiviwonder <sevicar3@gmail.com>
2020-11-17 16:17:21 +0100

    new process generator

---
+++ include/loader.h
@@ -0,0 +1,8 @@
+#ifndef loader_h
+#define loader_h
+
+#include "global.h"
+
+process_t create_process();
+void* loader(void *f_pgen);
+#endif
---
+++ src/loader.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include "../include/global.h"
+#include "../include/loader.h"
+#include "../include/tree.h"
+
+/*====== GLOBAL VARIABLES ======*/
+
+sem_t sem_pgen;
+node_t* root;
+volatile node_t* leftmost;
+pthread_mutex_t lock;
+volatile int num_process;
+
+/*====== FUNCTION IMPLEMENTATIONS ======*/
+
+process_t create_process(){
+
+    process_t p;
+
+    long pid = rand();			    // ID aleatoria sortu
+    int vruntime = rand() % 250 + 1;	    // virtual runtime aleatorioa sortu 
+    int time = vruntime + ((rand()%10)-5);  // Exekuzio denbora sortu, vruntime-en inguruan
+
+    // Prozesua hasieratu
+    p.pid = pid;
+    p.vruntime = vruntime;
+    p.time = time;
+
+    return p;
+}
+
+void* loader(void *f_pgen){
+    srand(time(0));
+
+    int p_tick = *(int*) f_pgen;    // Maiztasunaren parametroa jaso
+
+    // Hasieratze prozesua sortu zuhaitza hasieratzeko
+    process_t p;
+
+
+    p = create_process();
+    mm_t mm;
+    root = new_node(p, mm);
+    leftmost = root;
+    num_process = 0;
+
+    while(1){
+
+	// Erlojuarekin sinkronizazio semaforoa
+        sem_wait(&sem_pgen);
+
+	// Erloju tick-ak kontatu
+        p_tick--;
+        if (p_tick == 0){
+	    
+	    // Prozesua sortu
+            p = create_process();
+
+	    // Zuhaitzan sartu
+	    pthread_mutex_lock(&lock);
+	    insert(root, p);
+	    pthread_mutex_unlock(&lock);
+
+	    num_process++;
+
+	    DEBUG_WRITE("[PGEN] process created, num_process = %d\n", num_process);
+
+	    // Tick-ak kontatzeko tick pribatua berbiarazi
+            p_tick = *(int*) f_pgen;
+        }
+    }
+}
+
