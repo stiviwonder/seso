65055419569373e0233ca3e4f0c68201e82d3079
stiviwonder <sevicar3@gmail.com>
2020-11-17 16:21:17 +0100

    compilation stuff

--- lmake.lua
+++ lmake.lua
@@ -6,15 +6,15 @@ function build()
     lmake_set_compiler("/bin/gcc")
     lmake_set_compiler_flags("-pthread -Wall -Iinclude -DDEBUG")
     lmake_set_compiler_out("build/%.o")
-    lmake_compile("src/main.c src/func.c src/clock.c src/timer.c src/p_gen.c src/scheduler.c src/tree.c")
+    lmake_compile("src/main.c src/func.c src/clock.c src/timer.c src/loader.c src/scheduler.c src/tree.c")
     lmake_set_linker("/bin/gcc")
     lmake_set_linker_flags("-pthread")
     lmake_set_linker_out("seso")
-    lmake_link("build/main.c.o build/func.c.o build/clock.c.o build/timer.c.o build/p_gen.c.o build/scheduler.c.o build/tree.c.o")
+    lmake_link("build/main.c.o build/func.c.o build/clock.c.o build/timer.c.o build/loader.c.o build/scheduler.c.o build/tree.c.o")
 end
 
 function clean()
-    lmake_exec("rm -rf build/main.c.o build/func.c.o build/clock.c.o build/timer.c.o build/p_gen.c.o build/scheduler.c.o build/tree.c.o")
+    lmake_exec("rm -rf build/main.c.o build/func.c.o build/clock.c.o build/timer.c.o build/loader.c.o build/scheduler.c.o build/tree.c.o")
 end
 
 function run()
